35ffc953409006d2cd9b60165f26e5c1585f9d4b
wiliammbr <wiliammbr@gmail.com>
2022-11-27 17:27:11 +0000

    Update ThemeFunctions.php

--- ThemeFunctions.php
+++ ThemeFunctions.php
@@ -1,4 +1,5 @@
 // The code in this file helps me on improving SEO for my Blog inside the Theme Header file.
+// Since updated to 2.3.2 on 27 11 2022, it appears that I don't need that anymore.
 
 <?php if ( is_home() ) : ?>
   <h1 style="display: none;">WILIAMMBR</h1>
